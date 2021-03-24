fba3bbde48f4aa2e3e7cc0bb109dfaef06ad8dd6
Abhiraj Kale <abhirajkale1806@gmail.com>
2021-03-25 00:58:10 +0530

    config-db

--- routes/api.js
+++ routes/api.js
@@ -44,7 +44,7 @@ router.post('/signup', function(req, res, next) {
         if (err) throw err; // not connected!
       
         // Use the connection
-        connection.query("INSERT IGNORE INTO `heroku_2f4d6f8d48f57a4`.`user_info` (`id`, `name`, `password`, `pri_key`, `pub_key`, `phone`) VALUES ('?', '?', '?', '?', '?', '?')", [id,name,password,private_key,public_key,phone],function (error, results, fields) {
+        connection.query('INSERT IGNORE INTO `heroku_2f4d6f8d48f57a4`.`user_info` (`id`, `name`, `password`, `pri_key`, `pub_key`, `phone`) VALUES (?, ?, ?, ?, ?, ?)', [id,name,password,private_key,public_key,phone],function (error, results, fields) {
             console.log("Inserted data Results length: " + results.length)
             console.log(results);
           // When done with the connection, release it.
