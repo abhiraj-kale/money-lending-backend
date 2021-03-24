4c4ff8a6d5b8901f590ce2ee9ed5f1b44182f2b1
Abhiraj Kale <abhirajkale1806@gmail.com>
2021-03-25 00:47:29 +0530

    config-db

--- routes/api.js
+++ routes/api.js
@@ -1,14 +1,10 @@
 var express = require('express');
 var router = express.Router();
 const { v4: uuidv4 } = require('uuid');
-const crypto = require('crypto')
-const { publicKey, privateKey } = crypto.generateKeyPairSync("rsa", {
-	modulusLength: 1024,
-})
-console.log("Private key: " + privateKey.toString());
-console.log("Public key: " + publicKey.toString())
-
 var mysql = require('mysql');
+const NodeRSA = require('node-rsa');
+const key = new NodeRSA({ b: 1024 });
+
 var pool  = mysql.createPool({
   connectionLimit : 10,
   host            : process.env.HOST,
@@ -16,22 +12,12 @@ var pool  = mysql.createPool({
   password        : process.env.PASSWORD,
   database        : process.env.DATABASE
 });
+
 pool.getConnection(function(err, connection) {
     if (err) throw err; // not connected!
-  
-    // Use the connection
-    connection.query('SELECT * FROM user_info', function (error, results, fields) {
-        console.log("Results length: " + results.length)
-        console.log(results);
-      // When done with the connection, release it.
-      connection.release();
-  
-      // Handle error after the release.
-      if (error) throw error;
-  
-      // Don't use the connection here, it has been returned to the pool.
-    });
-  });
+    else console.log("Connected to DB")
+});
+
 router.post('/login', function(req, res, next) {
     var key = req.body.key;
     var password = req.body.password;
@@ -41,10 +27,15 @@ router.post('/login', function(req, res, next) {
 });
 
 router.post('/signup', function(req, res, next) {
-    var phone = req.body.phone;
-    var name = req.body.name;
-    var password = req.body.password;
-    var id = uuidv4();
+    const phone = req.body.phone;
+    const name = req.body.name;
+    const password = req.body.password;
+    // user id 
+    const id = uuidv4(); 
+    // creating asym keys
+    const private_key = key.exportKey("private");
+    const public_key = key.exportKey("public");
+
     console.log(id);
     console.log(phone);
     console.log(name);
@@ -53,8 +44,8 @@ router.post('/signup', function(req, res, next) {
         if (err) throw err; // not connected!
       
         // Use the connection
-        connection.query("INSERT IGNORE INTO `heroku_2f4d6f8d48f57a4`.`user_info` (`id`, `name`, `password`, `pri_key`, `pub_key`, `phone`) VALUES ('?', '?', '?', '?', '?', '?')", [],function (error, results, fields) {
-            console.log("Results length: " + results.length)
+        connection.query("INSERT IGNORE INTO `heroku_2f4d6f8d48f57a4`.`user_info` (`id`, `name`, `password`, `pri_key`, `pub_key`, `phone`) VALUES ('?', '?', '?', '?', '?', '?')", [id,name,password,private_key,public_key,phone],function (error, results, fields) {
+            console.log("Inserted data Results length: " + results.length)
             console.log(results);
           // When done with the connection, release it.
           connection.release();
