964d36daf134fd66852f2a6d32ded77dffa1bb00
Abhiraj Kale <abhirajkale1806@gmail.com>
2021-03-26 19:50:51 +0530

    db

--- routes/api.js
+++ routes/api.js
@@ -55,7 +55,7 @@ pool.getConnection(function(err, connection) {
 
 router.post('/login', function(req, response, next) {
     const id = req.body.id;
-    const auth_key = req.body.auth_key
+    const auth_key = JSON.stringify(req.body.auth_key).replace(" ","");
     console.log("ID : \n" + id);
     console.log("auth key : \n" + auth_key)
     let password;
