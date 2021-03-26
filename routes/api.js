5b81b1845eb7f4225eb66be1805818ac6c5419d4
Abhiraj Kale <abhirajkale1806@gmail.com>
2021-03-26 17:58:23 +0530

    db

--- routes/api.js
+++ routes/api.js
@@ -67,7 +67,7 @@ router.post('/login', function(req, response, next) {
       }
       password = crypto.createHash('md5').update(temp_pass).digest('hex');  
       
-      connection.query("SELECT `user_info`.`password` FROM `heroku_2f4d6f8d48f57a4`.`user_info` where `user_info`.`id`=?",[id],function(err, res){
+      connection.query("SELECT `user_info`.`password` FROM `heroku_2f4d6f8d48f57a4`.`user_info` where `user_info`.`id`="+id,function(err, res){
         if (err) throw err;
         console.log("result length : " + res.length);
         console.log(res);
