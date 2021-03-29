b2403bcf55ee13499bcfa6d5535db9a122f7b065
Abhiraj Kale <abhirajkale1806@gmail.com>
2021-03-29 23:43:52 +0530

    api

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
 
+var http = require('http'); //importing http
+
+function startKeepAlive() {
+    setInterval(function() {
+        var options = {
+            host: 'https://money-lending-app.herokuapp.com/',
+            port: 80,
+            path: '/'
+        };
+        http.get(options, function(res) {
+            res.on('data', function(chunk) {
+                try {
+                    // optional logging... disable after it's working
+                    console.log("HEROKU RESPONSE: " + chunk);
+                } catch (err) {
+                    console.log(err.message);
+                }
+            });
+        }).on('error', function(err) {
+            console.log("Error: " + err.message);
+        });
+    }, 30 * 60 * 1000); // load every 20 minutes
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.end("Server running")
 });
 
+startKeepAlive();
 module.exports = router;
