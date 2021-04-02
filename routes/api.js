10395617a97c75a6b2bff241eda3e95c7ddb7853
Abhiraj Kale <abhirajkale1806@gmail.com>
2021-04-02 13:28:41 +0530

    payment

--- routes/api.js
+++ routes/api.js
@@ -283,7 +283,8 @@ router.get('/lent', function(req, res){
             connection.query("SELECT `user_info`.`name`, `user_info`.`phone` FROM `heroku_2f4d6f8d48f57a4`.`user_info` where `user_info`.`id`=?",[result[i].receiver_id], function(err,results){
               if(err) throw err;
               console.log(results);
-              final_result.push(results[0]);
+              jsonObj = {"name":results[0].name, "phone":results[0].phone, "amount": result[0].amount};
+              final_result[i] = jsonObj;
             })
           }
             console.log("final result of id : "+final_result);
